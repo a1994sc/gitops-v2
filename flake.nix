8e94e4564810b54708ea5523e2eef080e9f0fcdd
Allen Conlon <software@conlon.dev>
2024-12-21 20:46:06 -0500

    Init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = 
+    inputs@{
+      nixpkgs,
+      self,
+      ...
+    }: 
+    let
+      arch = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in inputs.flake-utils.lib.eachSystem arch (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          jsonnet
+        ];
+      };
+    }
+  );
+}
