42c8fce56bf6c2a0d37ddaf2306108a24383fc67
Allen Conlon <software@conlon.dev>
2024-12-24 20:50:51 -0500

    Added renovate resource gather

---
+++ .github/hack/update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+readarray resourceMap < <(yq -o=j -I=0 '.projects[]' .github/hack/resources.json)
+
+for resource in "${resourceMap[@]}"; do
+  path=$(echo "$resource" | yq e '.path' -)
+  url=$(echo "$resource" | yq e '.repo.url' -)
+  tag=$(echo "$resource" | yq e '.repo.tag' -)
+  file=$(echo "$resource" | yq e '.repo.path' -)
+  release=$(echo "$resource" | yq e '.repo.release' -)
+
+  mkdir -p $path
+
+  if [ "$release" = "true" ]; then
+    gh release download --repo $url --pattern $file --dir $path --clobber $tag
+  else
+    echo "TODO: gh file from repo"
+  fi
+done
