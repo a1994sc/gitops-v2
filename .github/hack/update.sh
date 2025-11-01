44279553915eb52422e956b4a6a62331fa55d393
Allen Conlon <software@conlon.dev>
2025-11-01 18:09:33 -0400

    ci: clean up publishing

--- .github/hack/update.sh
+++ .github/hack/update.sh
@@ -9,11 +9,11 @@ for resource in "${resourceMap[@]}"; do
   file=$(echo "$resource" | yq e '.repo.path' -)
   tempDir="$(mktemp -d -t temp.XXXXXX)"
 
+  rm -rf $path
   mkdir -p $path
 
   if [ `echo "$resource" | yq e '.file.extract'` = "true" ]; then
     gh release download --repo $url --pattern $file --dir $tempDir --clobber $tag
-
     tar -xf $tempDir/$file -C $path --overwrite
   elif [ `echo "$resource" | yq e '.repo.release'` = "true" ]; then
     gh release download --repo $url --pattern $file --dir $path --clobber $tag
